be7b153b118bee807ff81e06e6da8cdf74f9968b
c3220162 <135084443+c3220162@users.noreply.github.com>
2025-03-26 09:01:18 +0000

    dmeo

--- dmeo/app.js
+++ dmeo/app.js
@@ -15,19 +15,3 @@ const addData = async () => {
 };
 
 addData();
-// app.js
-import db from './firebase';
-
-// データを取得
-const getData = async () => {
-  try {
-    const snapshot = await db.collection('users').get();
-    snapshot.forEach(doc => {
-      console.log(doc.id, doc.data());
-    });
-  } catch (error) {
-    console.error('エラー:', error);
-  }
-};
-
-getData();
