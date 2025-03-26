e1315822a95e338486db08064b00e68adf48d45c
c3220162 <135084443+c3220162@users.noreply.github.com>
2025-03-26 15:43:17 +0000

    a

--- dmeo/index.js
+++ dmeo/index.js
@@ -1,8 +1,9 @@
 import{firestore} from "./firebase.js"
-
+window.onload = async () => {
 const value = {
   birth: 1014,
   name: "yuuki",
   createdAt: new Date(),
 };
 await firestore.collection("user").add(value);
+};
