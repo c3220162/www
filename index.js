ee4fb3a324127a9c2a121c181bfbd12fc27d64ac
c3220162 <135084443+c3220162@users.noreply.github.com>
2025-03-27 01:31:12 +0000

    a

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-import { db } from "./firebase.js";
-const main = async () => {
-const value = {
-  birth: 1014,
-  name: "yuuki",
-  createdAt: new Date(),
-};
-await firestore.collection("user").add(value);
-};
+import { db,collection,addDoc } from "./firebase.js";
+async function addData() {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      name: "John Doe",
+      age: 30,
+      city: "Tokyo"
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
+}
+
+// 関数を実行
+addData();
